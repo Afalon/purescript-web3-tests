4444138fb70a47d30d02aaae2304edc599cc8112
Blinky <blinky3713@gmail.com>
2017-10-30 18:51:55 -0400

    get address with network id

--- src/Utils.purs
+++ src/Utils.purs
@@ -4,6 +4,7 @@ import Prelude
 
 import Control.Error.Util (note)
 import Control.Monad.Aff (Aff)
+--import Control.Monad.Eff.Class (liftEff)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Exception (EXCEPTION, throw)
 import Control.Monad.Except (runExcept)
@@ -12,18 +13,17 @@ import Data.Argonaut.Prisms (_Object, _String)
 import Data.Either (Either)
 import Data.EitherR (fmapL)
 import Data.Foreign (renderForeignError)
-import Data.Foreign.Class (decode)
-import Data.Foreign.JSON (parseJSON)
+import Data.Foreign.Class (decode, encode)
 import Data.Lens ((^?))
 import Data.Lens.Index (ix)
 import Data.Maybe (maybe)
 import Network.Ethereum.Web3.Api (net_version)
 import Network.Ethereum.Web3.Provider (httpProvider)
-import Network.Ethereum.Web3.Types (Address, ETH, Provider, fromHexString, runWeb3MA)
+import Network.Ethereum.Web3.Types (Address, ETH, Provider, runWeb3MA)
 import Node.Encoding (Encoding(UTF8))
 import Node.FS.Aff (FS, readTextFile)
 import Node.Process (PROCESS, lookupEnv)
-
+--import Control.Monad.Eff.Console (logShow)
 
 makeProvider :: forall eff . Eff (process :: PROCESS, eth :: ETH, exception :: EXCEPTION | eff) Provider
 makeProvider = do
@@ -31,15 +31,22 @@ makeProvider = do
   url <- maybe (throw "Must provide node url") pure murl
   httpProvider url
 
-
 getDeployedAddress :: forall eff . Provider -> String -> Aff (fs :: FS, eth :: ETH | eff) (Either String Address)
 getDeployedAddress p contractName = do
   let fname = "./build/contracts/" <> contractName <> ".json"
-  nodeId <- fromHexString <$> runWeb3MA p net_version
+  nodeId <- runWeb3MA p net_version
   ejson <- jsonParser <$> readTextFile UTF8 fname
   pure $ do
     contractJson <- ejson
     networks <- note "artifact missing networks key" $ contractJson ^? _Object <<< ix "networks"
-    addr <- note "artifact missing address" $ networks ^? _Object <<< ix "address" <<< _String
-    fmapL (show <<< map renderForeignError) <<< runExcept $ decode =<< parseJSON addr
+    net <- note ("artifact missing network: " <> show nodeId)  $ networks ^? _Object <<< ix (show nodeId)
+    addr <- note "artifact has no address" $ net ^? _Object <<< ix "address" <<< _String
+    fmapL (show <<< map renderForeignError) <<< runExcept <<< decode <<< encode $ addr
 
+{-
+foo = void <<< launchAff $ do
+  p <- liftEff makeProvider
+  r <- getDeployedAddress p "SimpleStorage"
+  liftEff $ logShow r
+  pure r
+-}
