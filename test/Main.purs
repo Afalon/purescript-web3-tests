f973203e7a690b7e8b79ce0a28ef77fccb434b3e
Blinky <blinky3713@gmail.com>
2017-10-31 13:55:51 -0400

    events test passing

--- test/Main.purs
+++ test/Main.purs
@@ -1,41 +1,10 @@
 module Test.Main where
 
 import Prelude
-import Utils (makeProvider, getDeployedContract, Contract(..))
-import Control.Monad.Eff.Console (CONSOLE, logShow)
-import Contracts.SimpleStorage as SimpleStorage
-import Control.Monad.Eff.Class (liftEff)
-import Control.Monad.Eff (Eff)
-import Data.Array ((!!))
+import Data.Maybe (Maybe(..))
+import SimpleStorageSpec (simpleStorageSpec)
 import Test.Spec.Reporter.Console (consoleReporter)
-import Test.Spec.Runner (RunnerEffects, run)
-import Data.Maybe (Maybe(..), fromJust)
-import Data.Symbol (SProxy(..))
-import Network.Ethereum.Web3.Types (ETH, embed, runWeb3MA)
-import Network.Ethereum.Web3.Api (eth_getAccounts)
-import Network.Ethereum.Web3.Solidity (uIntNFromBigNumber)
-import Test.Spec (Spec, describe, it)
-import Test.Spec.Assertions (shouldEqual)
-import Partial.Unsafe (unsafePartial)
+import Test.Spec.Runner (defaultConfig, run')
 
-main :: Eff (RunnerEffects _) Unit
-main = run [consoleReporter] $ do
+main = run' defaultConfig {timeout = Just (60 * 1000)} [consoleReporter] $ do
   simpleStorageSpec
-
-
-simpleStorageSpec :: forall r . Spec _ Unit
-simpleStorageSpec =
-  describe "interacting with a SimpleStorage Contract" do
-
-    it "can set the value of simple storage" do
-      provider <- liftEff makeProvider
-      accounts <- runWeb3MA provider eth_getAccounts
-      liftEff $ logShow accounts
-      let primaryAccount = unsafePartial $ fromJust $ accounts !! 0
-
-      Contract simpleStorage <- getDeployedContract provider (SProxy :: SProxy "SimpleStorage")
-      hx <- runWeb3MA provider $ do
-         let n = unsafePartial $ fromJust <<< uIntNFromBigNumber <<< embed $ 1
-         SimpleStorage.setCount (Just simpleStorage.address) primaryAccount (embed 0) n
-      liftEff $ logShow hx
-      true `shouldEqual` true
--- truffle.js
+++ truffle.js
@@ -4,6 +4,11 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    rinkeby: {
+        host: "geth-rinkeby-deploy.foam.svc.cluster.local",
+        port: 8545,
+        network_id: "*" // Match any network id
     }
   }
 };
