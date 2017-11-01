0ebbb099f630856046d2d0dc8826b8ff5fc73df4
Ilya Ostrovskiy <iostat@users.noreply.github.com>
2017-11-01 14:22:25 -0400

    Jenkinsfile and truffle.js to support it

--- truffle.js
+++ truffle.js
@@ -9,6 +9,11 @@ module.exports = {
         host: "geth-rinkeby-deploy.foam.svc.cluster.local",
         port: 8545,
         network_id: "*" // Match any network id
+    },
+    ephemeralnet: { // required for CI testing
+        host: "EPH_POD_NAME.geth-pods.jenkins.svc.cluster.local",
+        port: 8545,
+        network_id: "*"
     }
   }
 };
