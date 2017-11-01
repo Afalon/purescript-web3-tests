28626c6ba0ffcafe14bbfb4fa4ac2af41d355357
Blinky <blinky3713@gmail.com>
2017-11-01 10:21:37 -0400

    blow out

--- test/SimpleStorageSpec.purs
+++ test/SimpleStorageSpec.purs
@@ -3,14 +3,12 @@ module SimpleStorageSpec where
 import Prelude
 
 import Contracts.SimpleStorage as SimpleStorage
-import Control.Monad.Eff (Eff)
-import Control.Monad.Aff.Class (liftAff)
 import Control.Monad.Aff (launchAff)
 import Control.Monad.Aff.AVar (AVAR, makeEmptyVar, putVar, takeVar)
+import Control.Monad.Aff.Class (liftAff)
+import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Class (liftEff)
 import Control.Monad.Eff.Console (CONSOLE, logShow)
-import Node.FS.Aff (FS)
-import Node.Process (PROCESS)
 import Control.Monad.Eff.Exception (EXCEPTION)
 import Data.Array ((!!))
 import Data.Maybe (Maybe(..), fromJust)
@@ -19,10 +17,12 @@ import Network.Ethereum.Web3.Api (eth_getAccounts)
 import Network.Ethereum.Web3.Contract (EventAction(..), event)
 import Network.Ethereum.Web3.Solidity (uIntNFromBigNumber)
 import Network.Ethereum.Web3.Types (ETH, embed, forkWeb3MA, runWeb3MA)
+import Node.FS.Aff (FS)
+import Node.Process (PROCESS)
 import Partial.Unsafe (unsafePartial)
 import Test.Spec (Spec, describe, it)
-import Test.Spec.Runner (timeout)
 import Test.Spec.Assertions (shouldEqual)
+import Test.Spec.Runner (timeout)
 import Utils (makeProvider, getDeployedContract, Contract(..))
 
 simpleStorageSpec :: forall r . Spec _ Unit
@@ -38,10 +38,12 @@ simpleStorageSpec =
       let n = unsafePartial $ fromJust <<< uIntNFromBigNumber <<< embed $ 1
       hx <- runWeb3MA provider $
          SimpleStorage.setCount (Just simpleStorage.address) primaryAccount (embed 0) n
-      _ <- forkWeb3MA provider $
+      _ <- liftAff $ runWeb3MA provider $
         event provider simpleStorage.address $ \(SimpleStorage.CountSet _count) -> do
           liftEff $ logShow (_count)
           _ <- liftAff $ putVar _count var
+          liftEff $ logShow $ "Put count: " <> show _count
           pure TerminateEvent
+      liftEff $ logShow hx
       val <- takeVar var
       Just val `shouldEqual` Just n
