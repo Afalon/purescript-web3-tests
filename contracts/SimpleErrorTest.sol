4f92231c96c98c1f3d134f711c8e9b87c648b88f
blinky3713 <blinky@foam.space>
2018-02-02 22:40:38 -0500

    add error test contract

---
+++ contracts/SimpleErrorTest.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.4.18;
+
+contract SimpleErrorTest {
+    
+    mapping(uint => uint[2]) public table;
+    
+    string public name;
+    
+    function set(uint _key, uint _x, uint _y) public {
+        table[_key] = [_x,_y];
+    }
+    
+}
